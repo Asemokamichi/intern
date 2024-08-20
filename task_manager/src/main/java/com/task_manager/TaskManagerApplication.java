fd540e8bcb0250eb5fb20a47fa843d67a7897399
Asemokamichi <asemokamichi@gmail.com>
2024-08-20 14:55:38 +0500

    [#2] Added interaction of user and task microservices

--- task_manager/src/main/java/com/task_manager/TaskManagerApplication.java
+++ task_manager/src/main/java/com/task_manager/TaskManagerApplication.java
@@ -1,11 +1,16 @@
 package com.task_manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
